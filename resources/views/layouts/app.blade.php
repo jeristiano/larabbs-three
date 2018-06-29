bb16b7f3f4e4f9a0ab287f8f9f137bdd10c2f912
jeristiano <jeremykuang@foxmail.com>
2018-06-29 10:49:36 +0000

    editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
 </head>
 <body>
 <div id="app" class="{{ route_class() }}-page">
@@ -28,5 +29,6 @@
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
+@yield('scripts')
 </body>
 </html>
