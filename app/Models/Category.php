c4e05965de8f2cafbbd85d1703043447b7878b27
jeristiano <jeremykuang@foxmail.com>
2018-06-29 10:40:21 +0000

    user can create topics

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class Category extends Model
 {
-    protected $fillable=['name','description'];
+    protected $fillable = ['name', 'description'];
+
+    //使用cache缓存分类数据
+    public static function allCached()
+    {
+        $expiresAt = Carbon::now()->addMinutes(180);
+        return Cache::remember('categoryCached', $expiresAt, function()
+        {
+            return self::all();
+        });
+
+
+    }
 }
+
