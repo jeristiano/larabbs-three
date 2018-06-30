cb69dde54f93a35d825597eda28b7f19e49fcf1f
jeristiano <jeremykuang@foxmail.com>
2018-06-30 13:37:39 +0000

    notify author when topic replied

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -12,6 +13,8 @@ class ReplyObserver
     public function created(Reply $reply)
     {
         $reply->topic->increment('reply_count', 1);
+        // 通知作者话题被回复了
+        $reply->user->notify(new TopicReplied($reply));
     }
 
     public function creating(Reply $reply)
