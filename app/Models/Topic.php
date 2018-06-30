207e47fd3e491d52bda6cda4a3138e3e7a936a62
jeristiano <jeremykuang@foxmail.com>
2018-06-30 11:39:10 +0000

    list user replieds

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,16 +4,21 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable =['title', 'body', 'category_id', 'excerpt', 'slug'];
+    protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
 
     public function category()
     {
-     return   $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class);
     }
 
     public function user()
     {
-    return    $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
     }
 
     public function scopeWithOrder($query, $order)
---
+++ resources/views/topics/_reply_box.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JeremyK
+ * Date: 2018/06/30
+ * Time: 19:03
+ */
