6ba47f1ca60d66f3a7a0e99b8d4825a88cb4604c
jeristiano <jeremykuang@foxmail.com>
2018-07-01 10:57:27 +0000

    user can delete reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -19,7 +19,12 @@ class ReplyObserver
 
     public function creating(Reply $reply)
     {
-        $reply->content=clean($reply->content, 'user_topic_body');
+        $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decrement('reply_count',1);
     }
 
 }
